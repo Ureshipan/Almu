2802f509cf7db39a7e3ef3f5ffdc8bb1c442ef79
Ureshipan <tiptevlad@yandex.ru>
2022-02-17 21:25:53 +0300

    start

--- waiter.py
+++ waiter.py
@@ -11,17 +11,14 @@ while True:
     data = conn.recv(1024)
     if data:
         mes = json.loads(data.decode("utf-8"))['text']
+        print(mes)
         if mes.split()[0].lower() == 'найти':
-            string = ' '.join(mes.split()[1:])
+            string = '%'.join(mes.split()[1:])
             con = sqlite3.connect('tracks.db')
             cursor = con.cursor()
-            sql_request = '''
-            SELECT
-                title,
-                url
-            FROM tracks
-            WHERE MATCH(text) AGAINST (?);'''
-            cursor.execute(sql_request, string)
+            sql_request = 'SELECT title, url FROM tracks WHERE UPPER(text) LIKE "%{}%";'.format(string.upper())
+            print(sql_request)
+            cursor.execute(sql_request)
             rows = cursor.fetchall()
             cursor.close()
             con.close()
@@ -29,6 +26,7 @@ while True:
             for song in rows:
                 req['title'].append(song[0])
                 req['url'].append(song[1])
+            print(req)
             req = json.dumps(req)
             conn.sendall(bytes(req, encoding="utf-8"))
     conn.close()
